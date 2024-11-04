c01c6e1ab716d6d2423e096e92a2ad3d1c0eb633
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-11-04 14:02:12 +0530

    chnage

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,23 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// // export default defineConfig(({ mode }) => {
-// //   return {
-// //     base: '/CERAMIC_COMPLEX_FE/',
-// //     plugins: [react()],
-// //     build: {
-// //       chunkSizeWarningLimit: 1600,
-// //     },
-// //   }
-// // });
-
-// export default defineConfig(() => {
+// https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
 //   return {
-//     base: '/water_quality_system/',
+//     base: '/CERAMIC_COMPLEX_FE/',
 //     plugins: [react()],
 //     build: {
 //       chunkSizeWarningLimit: 1600,
 //     },
 //   }
 // });
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  return {
+    base: '/water_quality_system/',
+    plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
+  }
+});
